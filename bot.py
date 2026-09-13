a72269cdad2ba00c1d19bb4c72d19b270be3eb94
arghyadev8699-create <arghyadev8699@gmail.com>
2026-09-13 12:23:11 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,16 +37,14 @@ TOKEN_PANEL_MAP = {
     "H68v0LJ4ml5GCpiL6_HvQAeaB7HHqnfrIzCQfvAiGEA": "Ali nain",
     "ap1LzYsovP1mf1VN7Bb1y6i3X7u5kr1MMzZBsWtXm60": "K",
     
-    # NEW & Updated Tokens
+    # NEW Added Tokens
     "1a97dd85e7abfb93f5c54ca83185d08382cf0a86767f671227982a03e99fb01b": "Usman Baloch",
     "jM3NmxAshzg-YXUdSqaKqSNy-j57V12E2-qoToVgp6E": "Amir Jutt",
     "N2qbHY_60DJfNDoDoBNfblO2GpwxfMnoIG3zuCZJc9Q": "Khatoon",
-    "5IAx28HfLNxpCBwXCGXuVCa7pwOm58ec5oZrtqtTxSA": "Realistic",
+    "ZJOhLnAGM175HA_Vj7aFp1RvKEFO5x6z_GvjfwLvRII": "Realistic",
     "2vnz7Ge6LVxQWdSZ2oxL8A8ROXzCIS0eZkGp93h0u-o": "Dr Abaido",
     "QC-DX5NF1lCyLhEc7ma6YOcln9VkS277bx05Lrm7irU": "Hashim",
-    "cNIwFhps5oCDSRM-hCzHxUGcuwpY35UzdtqwKypRzJs": "Innocent",
-    "OJ3ZlYxop5y_xSwk5PAi6FiWwqDgMl7uQ2ZQsmUHBZs": "Syed Ahsan",
-    "8NOCNS1aa2zShmLKvhtNdKd6htlF0khKuiOWR1DwDNo": "Yasir Abbas",
+    "Q26YYl5yg19UVYhCZlRvgHtshHpTknVfaGhyaFZScHw=": "Rashid",
 }
 
 env_tokens = [t.strip() for t in os.environ.get("LAMIX_TOKEN", "").split(",") if t.strip()]
